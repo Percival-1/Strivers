57a04e434799612c876fd1c8127334498a003e2e
Percival-1 <shubhranshuranjan2004@gmail.com>
2025-01-07 13:50:06 +0530

    udate

---
+++ Stack and Queue/Queue using LL.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<cmath>
+#include<algorithm>
+#include<unordered_map>
+using namespace std;
+
+
+
+int main(){
+    
+    return 0;
+}
---
+++ Stack and Queue/Stack using LL.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<cmath>
+#include<algorithm>
+#include<unordered_map>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1)
+    {
+    data = data1;
+    next = nullptr;
+    }  
+};
+
+class Stack{
+    public:
+    Node* head;
+    int size = 0;
+
+    public:
+    Stack()
+    {
+        head = nullptr;
+    }
+
+    public:
+    void push(int n)
+    {
+      Node* mover = new Node(n);
+      mover->next = head;
+      head = mover;
+      size++;
+    }
+
+    public:
+    void pop()
+    {
+      Node* temp = head;
+      head = head->next;
+      delete temp;
+      size--;
+    }
+
+    public:
+    void peek()
+    {
+        cout<<"Top element : "<<head->data<<endl;
+    }
+
+    public:
+    void print(Node* head)
+    {
+        Node* mover = head;
+        while (mover!=NULL)
+        {
+            cout<<mover->data<<" ";
+            mover = mover->next;
+        }
+        cout<<endl;
+    }
+};
+
+int main(){
+    Stack* st = new Stack();
+    st->push(4);
+    st->push(5);
+    st->push(6);
+    st->push(7);
+    st->push(8);
+    st->pop();
+    st->print(st->head);
+    st->peek();
+    return 0;
+}
