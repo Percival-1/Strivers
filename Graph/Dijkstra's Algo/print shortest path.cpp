5cf563028e72ac9c38e1cbcf31e4fef2c6944273
Percival-1 <shubhranshuranjan2004@gmail.com>
2025-04-02 09:26:52 +0530

    update

--- Graph/Dijkstra's Algo/print shortest path.cpp
+++ Graph/Dijkstra's Algo/print shortest path.cpp
@@ -6,13 +6,26 @@
 #include<unordered_map>
 #include<stack>
 #include<queue>
+#include<set>
 using namespace std;
 
-vector<int> dij(vector<vector<pair<int , int>>> &arr , int s)
+vector<int> dij(vector<vector<pair<int , int>>> &arr ,int s)
 {
     int v = arr.size();
     vector<int> dis(v , INT_MAX);
+    dis[s] = 0;
     vector<int> path(v , 0);
+    path[s] = s;
+    set<pair<int , int>> st;
+    st.insert({0 , s});
+    while (!st.empty())
+    {
+        pair<int , int> p = *st.begin();
+        st.erase(st.begin());
+        int distance = p.first;
+        
+    }
+    
 }
 
 void adj(vector<vector<pair<int , int>>> &arr , int i , int j , int e)
